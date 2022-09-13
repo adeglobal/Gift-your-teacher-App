dc95a3b91ca2ac451ecafbb74fbcda24da19b82a
OlamireDev <komodo4real@yhoo.com>
2022-09-13 04:30:59 +0100

    Starting on SecurityConfig

---
+++ src/main/java/com/decagon/rewardyourteacherapi/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.decagon.rewardyourteacherapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception{
+        return security
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","index", "login","signup", "/css/*", "/js/*")
+                .permitAll()
+                .and().formLogin().and().build();
+    }
+}
