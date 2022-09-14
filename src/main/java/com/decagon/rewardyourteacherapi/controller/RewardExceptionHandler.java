02727e40566bf7ea1c4b53a8c63ecc96be7a742c
OlamireDev <komodo4real@yhoo.com>
2022-09-14 10:28:59 +0100

    Finished

--- src/main/java/com/decagon/rewardyourteacherapi/controller/RewardExceptionHandler.java
+++ src/main/java/com/decagon/rewardyourteacherapi/controller/RewardExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.decagon.rewardyourteacherapi.controller;
 
 import com.decagon.rewardyourteacherapi.exception.ApplicationException;
+import com.decagon.rewardyourteacherapi.payload.APIResponse;
+import com.decagon.rewardyourteacherapi.util.Responder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -8,7 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class RewardExceptionHandler {
 
     @ExceptionHandler(ApplicationException.class)
-    public String GenericException(ApplicationException ex){
-        return ex.getMessage();
+    public ResponseEntity<APIResponse> GenericException(ApplicationException ex){
+        Responder responder =new Responder();
+        return  responder.NotFound(ex.getMessage());
     }
 }
