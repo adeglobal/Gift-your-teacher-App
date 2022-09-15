a27f79702e018939774dbf7d1a688deaa94297ae
OlamireDev <komodo4real@yhoo.com>
2022-09-15 19:27:29 +0100

    Resolved conflicts with develop

--- src/main/java/com/decagon/rewardyourteacherapi/model/User.java
+++ src/main/java/com/decagon/rewardyourteacherapi/model/User.java
@@ -33,4 +33,12 @@ public class User {
         this.password = password;
         this.role = role;
     }
+    public User(long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
 }
