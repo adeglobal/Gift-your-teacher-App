ffa146ff6e1eb1e05282e973b0b12901b0639bef
OlamireDev <komodo4real@yhoo.com>
2022-09-15 17:00:04 +0100

    Resolved conflicts with develop

--- src/main/java/com/decagon/rewardyourteacherapi/exception/AuthorizationException.java
+++
@@ -1,7 +0,0 @@
-package com.decagon.rewardyourteacherapi.exception;
-
-public class AuthorizationException extends RuntimeException{
-    public AuthorizationException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/decagon/rewardyourteacherapi/payload/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.decagon.rewardyourteacherapi.payload;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String email;
-    private  String password;
-}
