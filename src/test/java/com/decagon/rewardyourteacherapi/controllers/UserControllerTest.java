5a1e45a0048fe53e0b2fff61fc506cff97344470
adeglobal <adeglobal2013@gmail.com>
2022-09-15 21:37:36 +0100

    login mock test

---
+++ src/test/java/com/decagon/rewardyourteacherapi/controllers/UserControllerTest.java
@@ -0,0 +1,51 @@
+package com.decagon.rewardyourteacherapi.controllers;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+import com.decagon.rewardyourteacherapi.payload.LoginDto;
+import com.decagon.rewardyourteacherapi.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ContextConfiguration(classes = {UserController.class})
+@ExtendWith(SpringExtension.class)
+class UserControllerTest {
+    @Autowired
+    private UserController userController;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void testLogin() throws Exception {
+        when(userService.login(any())).thenReturn(new ResponseEntity(HttpStatus.CONTINUE));
+
+        LoginDto loginDto = new LoginDto();
+        loginDto.setEmail("global@gmail.com");
+        loginDto.setPassword("1234");
+        String content = (new ObjectMapper()).writeValueAsString(loginDto);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/v1/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        Assertions.assertEquals(loginDto.getEmail(), "global@gmail.com");
+        Assertions.assertEquals(loginDto.getPassword(), "1234");
+        ResultActions actualPerformResult = MockMvcBuilders.standaloneSetup(userController).build().perform(requestBuilder);
+        actualPerformResult.andExpect(MockMvcResultMatchers.status().is(100));
+    }
+}
+
