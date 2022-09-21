206da3ba30e81e845306f71e12561fec07a4460e
Colemicha <adeniyimichael329@gmai.com>
2022-09-21 10:12:02 +0100

    Funding Student Wallet

--- src/main/java/com/decagon/rewardyourteacherapi/security/SecurityConfig.java
+++ src/main/java/com/decagon/rewardyourteacherapi/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests().antMatchers("/api/v1/login/**","/api/v1/register/**",
-                        "/index","/api/oauth2/student/callback","/app/**","/api/oauth2/teacher/callback","/api/v1/student/**")
+                        "/index","/api/oauth2/student/callback","/app/**", "/api/v1/student/**","/api/oauth2/teacher/callback")
                 .permitAll()
                 .antMatchers("/api/**").authenticated().and()
                 .exceptionHandling().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
