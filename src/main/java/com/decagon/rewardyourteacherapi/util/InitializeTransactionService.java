13bc77d3e6ccda2d9d3f515dc57ee666b38589d5
beny1122war <tyavbarnabas2@gmail.com>
2022-09-21 20:26:39 +0100

    Integrated verified paystack transaction

--- src/main/java/com/decagon/rewardyourteacherapi/util/InitializeTransactionService.java
+++ src/main/java/com/decagon/rewardyourteacherapi/util/InitializeTransactionService.java
@@ -1,11 +1,13 @@
 package com.decagon.rewardyourteacherapi.util;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -19,9 +21,7 @@ import java.io.InputStreamReader;
 @Transactional
 @AllArgsConstructor
 public class InitializeTransactionService {
-    private final PayStackVerifyTransactionResponse payStackVerifyTransactionResponse;
-
-    public String initTransaction(InitializeTransactionRequest request) throws Exception {
+    public InitializeTransactionResponse initTransaction(InitializeTransactionRequest request) throws Exception {
         InitializeTransactionResponse initializeTransactionResponse = null;
         try {
             // convert transaction to json then use it as a body to post json
@@ -53,14 +53,63 @@ public class InitializeTransactionService {
             ex.printStackTrace();
             throw new Exception("Failure initializaing paystack transaction");
         }
-        String test = initializeTransactionResponse.getData().getReference();
-        PayStackVerifyTransactionResponse payStackVerifyTransactionResponse1 = payStackVerifyTransactionResponse
-                .verifyTransaction(initializeTransactionResponse.getData().getReference());
+        VerifyTransactionResponse payStackVerifyTransactionResponse1 =verifyTransaction(initializeTransactionResponse.getData().getReference());
         if(payStackVerifyTransactionResponse1 == null){
             throw new Exception("Failure initializaing paystack transaction");
         }
 
-        return "Payment Successful";
+        return initializeTransactionResponse;
+    }
+
+//Verify transaction
+    public static VerifyTransactionResponse verifyTransaction(String reference) throws Exception {
+
+        VerifyTransactionResponse payStackResponse;
+
+        try {
+
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet("https://api.paystack.co/transaction/verify/" + reference);
+            request.addHeader("Content-type", "application/json");
+            request.addHeader("Authorization", "Bearer sk_test_4eca7780fd55f4831d30f722d67e03ee49e8278d");
+
+            StringBuilder result = new StringBuilder();
+
+            HttpResponse response = client.execute(request);
+
+            if (response.getStatusLine().getStatusCode() == 200) {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+
+            } else {
+                throw new Exception("Error Occurred while connecting to pay-stack url");
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            try {
+                payStackResponse = mapper.readValue(result.toString(), VerifyTransactionResponse.class);
+
+
+                if (payStackResponse.getStatus().equals("success"))
+                    return payStackResponse;
+
+            } catch (JsonProcessingException e) {
+                System.err.println("You've already made payment or An error occurred while verifying payment ");
+                return null;
+            }
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new Exception("Internal server error");
+        }
+
+        return payStackResponse;
     }
 
 }
